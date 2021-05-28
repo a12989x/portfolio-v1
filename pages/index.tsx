247dd46934282b11a640923ba63d47f57b378f83
Alexis Guzman <alexis12989x@gmail.com>
2021-05-28 09:15:41 -0500

    feat(typescript): change file extensions from js to tsx
    Change files extension for files from .js to .tsx and add JSDocs documentation

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * Home: The landing page of web app
+ * @return {JSX.Element} The JSX Code for the Home page component
+ */
+const Home = (): JSX.Element => {
+	return (
+		<div>
+			<p>Home Page Component</p>
+		</div>
+	);
+};
+
+export default Home;
