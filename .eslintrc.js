cba0b966ebb5fd550ff983a51065d246fc240032
Alexis Guzman <alexis12989x@gmail.com>
2021-05-28 09:18:55 -0500

    style: fomrat files with prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-    'prettier',
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-  },
-  'settings': {'react': {'version': 'latest'}},
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'google',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'react/react-in-jsx-scope': 0,
+		'react/jsx-filename-extension': [1, { extensions: ['ts', 'tsx'] }],
+	},
+	settings: { react: { version: 'latest' } },
 };
