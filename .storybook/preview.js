da3e10c14375c7bdb1887b862641d4751ae277c1
Alexis Guzman <alexis12989x@gmail.com>
2021-05-31 09:44:25 -0500

    chore(storybook): set up sorybook with main and preview files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../styles/theme';
+
+export const decorators = [
+	(Story) => (
+		<ChakraProvider resetCSS theme={theme}>
+			<Story />
+		</ChakraProvider>
+	),
+];
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	controls: {
