999ef6f6f0e9f601ad987a30a37396a334d37ae7
Alexis Guzman <alexis12989x@gmail.com>
2021-06-02 14:15:54 -0500

    feat: delete button for toggle dark-mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import NextLink from 'next/link';
 import { Flex, Heading, Link, Text } from '@chakra-ui/layout';
-import { useColorMode } from '@chakra-ui/color-mode';
-import { Button } from '@chakra-ui/button';
 
 /**
  * Home: The landing page of web app
  * @return {JSX.Element} The JSX Code for the Home page component
  */
 const Home = (): JSX.Element => {
-	const { colorMode, toggleColorMode } = useColorMode();
-
 	return (
 		<Flex
 			as='main'
@@ -28,10 +24,6 @@ const Home = (): JSX.Element => {
 			<NextLink href='https://github.com/a12989x/' passHref>
 				<Link isExternal>Go to my GitHub</Link>
 			</NextLink>
-
-			<Button onClick={toggleColorMode}>
-				Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-			</Button>
 		</Flex>
 	);
 };
