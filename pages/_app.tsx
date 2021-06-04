a7624bdcc651ca29aef48f066576b5ac0b8dbc90
Alexis Guzman <alexis12989x@gmail.com>
2021-06-04 10:58:56 -0500

    feat: import layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 import theme from '@/styles/theme';
+import Layout from '@/components/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
 	return (
 		<ChakraProvider resetCSS theme={theme}>
-			<Component {...pageProps} />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</ChakraProvider>
 	);
 };
