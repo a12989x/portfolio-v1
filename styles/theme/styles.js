517bb8c7e92c8498e93213e320bb7c2d3d9d6810
Alexis Guzman <alexis12989x@gmail.com>
2021-06-04 13:12:26 -0500

    feat(theme): change the max width for __next id from 1000px to 1200px

--- styles/theme/styles.js
+++ styles/theme/styles.js
@@ -8,7 +8,7 @@ const styles = {
 			p: '1rem 2rem',
 			mx: 'auto',
 			minHeight: 'calc(100vh - 1rem)',
-			maxW: '1000px',
+			maxW: '1200px',
 			display: 'grid',
 			gridTemplate: 'auto 1fr auto / 1fr',
 		},
