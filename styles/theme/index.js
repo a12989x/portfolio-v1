b8f9c33ee57666bbb7e826c331a5726396cb4512
Alexis Guzman <alexis12989x@gmail.com>
2021-06-04 10:44:41 -0500

    chore(theme): delete Text and textStyles

--- styles/theme/index.js
+++ styles/theme/index.js
@@ -5,20 +5,17 @@ import styles from './styles';
 
 // Foundational style overrides
 import fonts from './foundations/fonts';
-import textStyles from './foundations/textStyles';
 import colors from './foundations/colors';
 
 // Component style overrides
 import Button from './components/button';
 import Link from './components/link';
-import Text from './components/text';
 
 const overrides = {
 	styles,
 	fonts,
-	textStyles,
 	colors,
-	components: { Button, Link, Text },
+	components: { Button, Link },
 };
 
 const theme = extendTheme(overrides);
