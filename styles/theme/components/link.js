3c9e41e247b8b319243610d6d5930eb7b2679f67
Alexis Guzman <alexis12989x@gmail.com>
2021-06-04 10:39:47 -0500

    chore: delete variants

--- styles/theme/components/link.js
+++ styles/theme/components/link.js
@@ -1,32 +1,11 @@
 const Link = {
-	baseStyle: {
-		textDecoration: 'underline',
-	},
+	baseStyle: {},
 	variants: {
 		default: (props) => ({
 			color:
 				props.colorMode === 'light' ? 'primary.500' : 'secondary.300',
 		}),
-		noStyle: {
-			textDecoration: 'none',
-			_hover: {
-				textDecoration: 'none',
-			},
-		},
 		subtle: (props) => ({
-			color:
-				props.colorMode === 'light' ? 'neutral.600' : 'default.light',
-			textDecoration: 'none',
-			_hover: {
-				color:
-					props.colorMode === 'light'
-						? 'default.dark'
-						: 'neutral.200',
-				textDecoration: 'none',
-			},
-		}),
-		large: (props) => ({
-			fontSize: '1.25rem',
 			color:
 				props.colorMode === 'light' ? 'neutral.600' : 'default.light',
 			textDecoration: 'none',
