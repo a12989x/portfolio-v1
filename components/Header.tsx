8c2eeed7214737648a8ece468c682dd1be23a9ca
Alexis Guzman <alexis12989x@gmail.com>
2021-06-04 10:54:59 -0500

    feat: create and set up Header component

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import { IconButton } from '@chakra-ui/button';
+import { useColorMode } from '@chakra-ui/color-mode';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { Flex, HStack } from '@chakra-ui/layout';
+
+import { LogoIcon } from '@/styles/theme/icons';
+import NavItem from './NavItem';
+
+const Header = (): JSX.Element => {
+	const { colorMode, toggleColorMode } = useColorMode();
+
+	return (
+		<Flex mb={5} as='header' p='4' justify='space-between'>
+			<LogoIcon boxSize={10} />
+			<HStack as='nav' spacing={8}>
+				<Flex as='ul' display={{ base: 'none', lg: 'flex' }}>
+					<NavItem href='/'>Home</NavItem>
+					<NavItem href='/about'>About</NavItem>
+					<NavItem href='/projects'>Projects</NavItem>
+					<NavItem href='/blog'>Blog</NavItem>
+				</Flex>
+
+				<IconButton
+					borderRadius='sm'
+					variant='icon'
+					onClick={toggleColorMode}
+					aria-label={
+						colorMode === 'light'
+							? 'Toggle dark mode'
+							: 'Toggle light Mode'
+					}
+					icon={
+						colorMode === 'light' ? (
+							<MoonIcon size='1.25rem' />
+						) : (
+							<SunIcon size='1.25rem' />
+						)
+					}
+				/>
+			</HStack>
+		</Flex>
+	);
+};
+
+export default Header;
