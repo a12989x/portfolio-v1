56d1619e2cfbc50d5fe159577c1d2f90394e8a48
Alexis Guzman <alexis12989x@gmail.com>
2021-06-10 16:16:05 -0500

    feat: create subscribe api with buttondown

---
+++ pages/api/subscribe.tsx
@@ -0,0 +1,67 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = { success?: boolean; message: string };
+
+const { BUTTONDOWN_API_KEY } = process.env;
+
+const subscribe = async (
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+): Promise<void> => {
+	const { email } = JSON.parse(req.body);
+
+	if (!email) {
+		return res
+			.status(400)
+			.json({ success: false, message: 'Email is required' });
+	}
+
+	try {
+		const body = JSON.stringify({
+			email,
+			tags: ['codaxx.ml'],
+			referrer_url: 'codaxx.ml',
+		});
+
+		const options = {
+			body,
+			headers: {
+				Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+		};
+
+		const data = await fetch(
+			`https://api.buttondown.email/v1/subscribers`,
+			options
+		);
+
+		if (data.status >= 400) {
+			const text = await data.text();
+
+			if (text.includes('already subscribed')) {
+				return res.status(400).json({
+					success: true,
+					message: `You've already subscribed!`,
+				});
+			}
+
+			return res.status(400).json({
+				success: false,
+				message: 'Your email is invalid.',
+			});
+		}
+
+		return res.status(201).json({
+			success: true,
+			message: "Congrats! You're on the mailing list.",
+		});
+	} catch (err) {
+		return res
+			.status(500)
+			.json({ success: false, message: 'Unable to subscribe. Sorry.' });
+	}
+};
+
+export default subscribe;
