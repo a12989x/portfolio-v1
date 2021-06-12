d6c8fb34b4fff5d5931ce0e663dc4b60250a1913
Alexis Guzman <alexis12989x@gmail.com>
2021-06-12 12:23:16 -0500

    feat: add custom hooks for toggle menu and conditional with media query hook

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,22 @@
 import { ReactNode } from 'react';
 
+import useToggleMenu from '@/utils/hooks/useToggleMenu';
+import { useMediaQuery } from '@chakra-ui/media-query';
+
 import Head from './Head';
 import Header from './header';
 import Footer from './footer';
+import MobileNav from './header/MobileNav';
 
 const Layout = ({ children }: { children: ReactNode }): JSX.Element => {
+	const { isMenuOpen, toggleMenu } = useToggleMenu();
+	const [isSmallScreen] = useMediaQuery('(max-width: 479px)');
+
 	return (
 		<>
 			<Head />
-			<Header />
-			{children}
+			<Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
+			{isMenuOpen && isSmallScreen ? <MobileNav /> : children}
 			<Footer />
 		</>
 	);
