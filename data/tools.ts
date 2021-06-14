d54c4e4df2563e783eb4c1a0db4a62d55550abd0
Alexis Guzman <alexis12989x@gmail.com>
2021-06-14 17:15:23 -0500

    chore: add alan ai object and fix id's

--- data/tools.ts
+++ data/tools.ts
@@ -10,6 +10,7 @@ import {
 	FirebaseIcon,
 	FigmaIcon,
 	SassIcon,
+	AlanAiIcon,
 } from '@/styles/theme/icons';
 
 import ToolInterface from '@/interfaces/Tool';
@@ -39,54 +40,61 @@ export const react: ToolInterface = {
 	name: 'React',
 	icon: ReactIcon,
 	color: '#61DAFB',
-	id: 4,
+	id: 3,
 };
 
 export const nodejs: ToolInterface = {
 	name: 'Node.js',
 	icon: NodeIcon,
 	color: '#339933',
-	id: 5,
+	id: 4,
 };
 
 export const nextjs: ToolInterface = {
 	name: 'Next.js',
 	icon: NextIcon,
 	color: 'currentColor',
-	id: 6,
+	id: 5,
 };
 
 export const chakra: ToolInterface = {
 	name: 'Chakra UI',
 	icon: ChakraIcon,
 	color: '#319795',
-	id: 9,
+	id: 6,
 };
 
 export const storybook: ToolInterface = {
 	name: 'Storybook',
 	icon: StorybookIcon,
 	color: '#FF4785',
-	id: 10,
+	id: 7,
 };
 
 export const firebase: ToolInterface = {
 	name: 'Sass',
 	icon: FirebaseIcon,
 	color: '#FFCA28',
-	id: 13,
+	id: 8,
 };
 
 export const figma: ToolInterface = {
 	name: 'Figma',
 	icon: FigmaIcon,
 	color: '#F24E1E',
-	id: 8,
+	id: 9,
 };
 
 export const sass: ToolInterface = {
 	name: 'Sass',
 	icon: SassIcon,
 	color: '#CC6699',
-	id: 13,
+	id: 10,
+};
+
+export const alanai: ToolInterface = {
+	name: 'Alan AI',
+	icon: AlanAiIcon,
+	color: '#32acff',
+	id: 11,
 };
