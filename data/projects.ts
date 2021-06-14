f3b57e7475070ed1ce43eac314615007dda7d82c
Alexis Guzman <alexis12989x@gmail.com>
2021-06-14 17:36:29 -0500

    chore: add voice control news project object

--- data/projects.ts
+++ data/projects.ts
@@ -1,6 +1,14 @@
-import { chakra, nextjs, nodejs, react, storybook } from './tools';
+import {
+	alanai,
+	chakra,
+	nextjs,
+	nodejs,
+	react,
+	sass,
+	storybook,
+} from './tools';
 
-import { ReexJsIcon, LogoIcon } from '@/styles/theme/icons';
+import { ReexJsIcon, LogoIcon, NewspaperIcon } from '@/styles/theme/icons';
 import ProjectsInterface from '@/interfaces/Projects';
 
 const projects: Array<ProjectsInterface> = [
@@ -19,13 +27,25 @@ const projects: Array<ProjectsInterface> = [
 	{
 		logo: LogoIcon,
 		title: 'Portfolio V1',
-		description: 'Website as a portfolio with a blog.',
-		tools: [react, nextjs, storybook, chakra],
-		live: 'https://codaxz.ml',
+		description:
+			'Personal website version one with a my own blog and newsletter.',
+		tools: [nextjs, storybook, chakra],
+		live: 'https://codaxx.ml/',
 		proto: null,
 		repo: 'https://github.com/a12989x/portfolio-v1/',
 		id: 2,
 		feature: true,
+	},
+	{
+		logo: NewspaperIcon,
+		title: 'Voice Control News',
+		description: 'Website as a portfolio with a blog.',
+		tools: [react, alanai, sass],
+		live: 'https://voice-control-react-news-app.vercel.app/',
+		proto: null,
+		repo: 'https://github.com/a12989x/voice-control-react-news-app/',
+		id: 2,
+		feature: false,
 	},
 ];
 
