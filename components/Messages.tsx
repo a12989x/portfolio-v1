f475d61e93e4a8a15756a3690e790aa418632dac
Alexis Guzman <alexis12989x@gmail.com>
2021-06-18 18:16:03 -0500

    feat: add spread props as property

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,25 +1,36 @@
-import { CheckCircleIcon, WarningIcon } from '@chakra-ui/icons';
-import { HStack, Text } from '@chakra-ui/layout';
 import React, { ReactNode } from 'react';
+import { CheckCircleIcon, WarningIcon } from '@chakra-ui/icons';
+import { HStack, StackProps, Text } from '@chakra-ui/layout';
 
 import useColorModeValues from '@/utils/hooks/useColorModeValues';
 
-const SuccessMessage = ({ children }: { children: ReactNode }): JSX.Element => {
+interface MessagesInterface extends StackProps {
+	children: ReactNode;
+	props?: StackProps;
+}
+
+const SuccessMessage = ({
+	children,
+	...props
+}: MessagesInterface): JSX.Element => {
 	const { successColor } = useColorModeValues();
 
 	return (
-		<HStack spacing={2}>
+		<HStack spacing={2} {...props}>
 			<CheckCircleIcon color={successColor} />
 			<Text color={successColor}>{children}</Text>
 		</HStack>
 	);
 };
 
-const ErrorMessage = ({ children }: { children: ReactNode }): JSX.Element => {
+const ErrorMessage = ({
+	children,
+	...props
+}: MessagesInterface): JSX.Element => {
 	const { errorColor } = useColorModeValues();
 
 	return (
-		<HStack spacing={2}>
+		<HStack spacing={2} {...props}>
 			<WarningIcon color={errorColor} />
 			<Text color={errorColor}>{children}</Text>
 		</HStack>
