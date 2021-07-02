0db2ff97a6dc5835e3c7a2ebb07668a8fb0452ac
Alexis Guzman <alexis12989x@gmail.com>
2021-07-02 13:27:17 -0500

    feat: create and set up dynamic blog page component

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,53 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { MDXRemote } from 'next-mdx-remote';
+import { Box, Heading } from '@chakra-ui/react';
+import { ParsedUrlQuery } from 'querystring';
+
+import { getPostBySlug, getPosts } from '@/utils/mdx';
+
+import MDXComponents from '@/components/mdx-components';
+import PostDataInterface from '@/interfaces/PostDataInterface';
+
+interface IParams extends ParsedUrlQuery {
+	slug: string;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const posts = await getPosts();
+	const paths = posts.map((p) => ({
+		params: { slug: p.replace(/\.mdx/, '') },
+	}));
+
+	return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	const { slug } = context.params as IParams;
+
+	const { mdxSource, frontMatter } = await getPostBySlug(slug);
+
+	return { props: { mdxSource, frontMatter } };
+};
+
+const Blog = ({ mdxSource, frontMatter }: PostDataInterface): JSX.Element => {
+	return (
+		<Box as='main' mx='auto' maxW='800px' w='100%'>
+			<Heading
+				as='h1'
+				size='2xl'
+				mt={12}
+				mb={20}
+				mx='auto'
+				maxW='container.sm'
+			>
+				{frontMatter.title}
+			</Heading>
+
+			<Box as='section'>
+				<MDXRemote {...mdxSource} components={MDXComponents} />
+			</Box>
+		</Box>
+	);
+};
+
+export default Blog;
