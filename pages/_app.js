105758fe8eb5aa221ebaeb571c3e18e2a91544de
Alexis Guzman <alexis12989x@gmail.com>
2021-07-09 13:40:57 -0500

    feat: install @badrap/bar-of-progress dependencie

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Router from 'next/router';
+import ProgressBar from '@badrap/bar-of-progress';
 import { ChakraProvider } from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
 
@@ -6,6 +8,17 @@ import SEO from '../next-seo.config';
 import theme from '@/styles/theme';
 import Layout from '@/components/Layout';
 import Head from '@/components/Head';
+
+const progress = new ProgressBar({
+	size: 2,
+	color: '#805AD5',
+	className: 'bar-of-progress',
+	delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
