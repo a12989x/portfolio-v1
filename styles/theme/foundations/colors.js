7fe1aae48649922a769f65726a2fb9c009140121
Alexis Guzman <alexis12989x@gmail.com>
2021-07-09 17:37:09 -0500

    chore(theme): remove unnecessary colors

--- styles/theme/foundations/colors.js
+++ styles/theme/foundations/colors.js
@@ -1,20 +1,14 @@
 const colors = {
 	default: { light: '#fcfcfc', dark: '#232323' },
 	primary: {
-		100: '#D1C1F4',
-		200: '#B199E7',
-		300: '#9C7DE4',
-		400: '#8F67D7',
 		500: '#805AD5',
 		600: '#744EC8',
-		700: '#311968',
 	},
 	neutral: {
 		100: '#F4F4F4',
 		200: '#D9D9D9',
 		300: '#A6A6A6',
 		400: '#8C8C8C',
-		500: '#737373',
 		600: '#595959',
 		700: '#404040',
 		800: '#303030',
@@ -22,9 +16,9 @@ const colors = {
 	},
 	accent: {
 		successLight: '#68D391',
-		successDark: '#276749',
+		successDark: '#04ae5f',
 		errorLight: '#FC8181',
-		errorDark: '#AA0000',
+		errorDark: '#ff1d1d',
 	},
 };
 
