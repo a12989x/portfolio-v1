f849609ac9fed7d8c318003b157e03812b45dac0
Alexis Guzman <alexis12989x@gmail.com>
2021-07-13 10:47:00 -0500

    feat(animations): add exit animation with animate presence and exit before enter

--- components/header/ToggleButton.js
+++ components/header/ToggleButton.js
@@ -1,26 +1,44 @@
 import { IconButton } from '@chakra-ui/button';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { useColorMode } from '@chakra-ui/color-mode';
+
+const MoonIconBox = motion(MoonIcon);
+const SunIconBox = motion(SunIcon);
 
 const ToggleButton = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
 
 	return (
-		<IconButton
-			borderRadius='sm'
-			onClick={toggleColorMode}
-			aria-label={
-				colorMode === 'light' ? 'Toggle dark mode' : 'Toggle light Mode'
-			}
-			icon={
-				colorMode === 'light' ? (
-					<MoonIcon size={6} />
-				) : (
-					<SunIcon size={6} />
-				)
-			}
-		/>
+		<AnimatePresence exitBeforeEnter>
+			<IconButton
+				borderRadius='sm'
+				onClick={toggleColorMode}
+				aria-label={
+					colorMode === 'light'
+						? 'Toggle dark mode'
+						: 'Toggle light Mode'
+				}
+				icon={
+					colorMode === 'light' ? (
+						<MoonIconBox
+							size={6}
+							initial={{ x: 20, opacity: 0 }}
+							animate={{ x: 0, opacity: 1 }}
+							exit={{ x: -20, opacity: 0 }}
+						/>
+					) : (
+						<SunIconBox
+							size={6}
+							initial={{ x: 20, opacity: 0 }}
+							animate={{ x: 0, opacity: 1 }}
+							exit={{ x: -20, opacity: 0 }}
+						/>
+					)
+				}
+			/>
+		</AnimatePresence>
 	);
 };
 
