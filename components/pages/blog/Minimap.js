82b22b39382518d43415d9b40f465697eebb36f7
Alexis Guzman <alexis12989x@gmail.com>
2021-11-17 17:59:36 -0600

    feat(blog): use media query custom hook on minimap component

--- components/pages/blog/Minimap.js
+++ components/pages/blog/Minimap.js
@@ -1,36 +1,41 @@
-import useColorModeValues from '@/hooks/useColorModeValues';
 import { Box } from '@chakra-ui/layout';
+
+import useColorModeValues from '@/hooks/useColorModeValues';
+import useMediaQueries from '@/hooks/useMediaQueries';
 
 const Minimap = () => {
   const { bgColor, greyColor } = useColorModeValues();
+  const { isExtraLargeSize } = useMediaQueries();
 
   return (
-    <Box
-      as='minimap'
-      display='block'
-      position='absolute'
-      top='5.75rem'
-      right='2rem'
-      height='calc(100% - 20rem)'
-    >
+    isExtraLargeSize && (
       <Box
-        className='screen-image'
-        position='sticky'
-        padding='1rem'
-        top='1rem'
-        bottom='1rem'
-        border='1px solid'
-        borderColor={greyColor}
-        bgColor={bgColor}
+        as='minimap'
+        display='block'
+        position='absolute'
+        top='5.75rem'
+        right='2rem'
+        height='calc(100% - 20rem)'
       >
         <Box
-          className='canvas'
-          height='250px'
-          width='90px'
-          background='transparent -moz-element(#main) no-repeat scroll center center / contain'
-        ></Box>
+          className='screen-image'
+          position='sticky'
+          padding='1rem'
+          top='1rem'
+          bottom='1rem'
+          border='1px solid'
+          borderColor={greyColor}
+          bgColor={bgColor}
+        >
+          <Box
+            className='canvas'
+            height='250px'
+            width='90px'
+            background='transparent -moz-element(#main) no-repeat scroll center center / contain'
+          ></Box>
+        </Box>
       </Box>
-    </Box>
+    )
   );
 };
 
