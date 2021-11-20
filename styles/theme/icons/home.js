04d49c54bef3ab587a42c41a408ad0466ef0d674
Alexis Guzman <alexis12989x@gmail.com>
2021-11-20 12:02:47 -0600

    feat(styles): create home icon

---
+++ styles/theme/icons/home.js
@@ -0,0 +1,14 @@
+import Icon from '@chakra-ui/icon';
+
+const HomeIcon = (props) => (
+  <Icon
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    {...props}
+  >
+    <path d='M10.7 2.3a1 1 0 0 0-1.4 0l-7 7a1 1 0 0 0 1.4 1.4l.3-.29V17a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-6.59l.3.3a1 1 0 0 0 1.4-1.42l-7-7z' />
+  </Icon>
+);
+
+export default HomeIcon;
