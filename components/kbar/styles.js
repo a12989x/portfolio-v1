aba5aac2a1367da973f45dbd399028e8fb767c1f
Alexis Guzman <alexis12989x@gmail.com>
2021-11-20 12:07:20 -0600

    feat(kbar): create syles for search and animator components

---
+++ components/kbar/styles.js
@@ -0,0 +1,21 @@
+const styles = {
+  search: {
+    padding: '12px 16px',
+    fontSize: '16px',
+    width: '100%',
+    boxSizing: 'border-box',
+    outline: 'none',
+    border: 'none',
+    background: '#805AD5',
+    color: '#fcfcfc',
+  },
+  animator: {
+    maxWidth: '600px',
+    width: '100%',
+    borderRadius: '0.125rem',
+    overflow: 'hidden',
+    boxShadow: '0px 6px 20px rgb(0 0 0 / 15%)',
+  },
+};
+
+export default styles;
