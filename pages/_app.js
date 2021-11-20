ebc95964b12976716b073e7212b186d57713fef8
Alexis Guzman <alexis12989x@gmail.com>
2021-11-20 12:13:48 -0600

    feat(app): import kbar dependencie, kabar components and confifure actions

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import ProgressBar from '@badrap/bar-of-progress';
 import { ChakraProvider } from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
+import {
+  KBarAnimator,
+  KBarPortal,
+  KBarPositioner,
+  KBarProvider,
+  KBarSearch,
+} from 'kbar';
 
 import SEO from '../next-seo.config';
 
 import theme from '@/styles/theme';
 import Layout from '@/components/Layout';
 import Head from '@/components/Head';
+import RenderResults from '@/components/kbar/RenderResults';
+import styles from '@/components/kbar/styles';
+import { HomeIcon } from '@/styles/theme/icons';
 
 const progress = new ProgressBar({
   size: 2,
@@ -21,14 +31,104 @@ Router.events.on('routeChangeComplete', progress.finish);
 Router.events.on('routeChangeError', progress.finish);
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const { search, animator } = styles;
+
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Layout>
-        <Head />
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <KBarProvider
+      actions={[
+        {
+          id: 'homeAction',
+          name: 'Home',
+          shortcut: ['h'],
+          keywords: 'back',
+          section: 'Navigation',
+          perform: () => router.push('/'),
+          icon: <HomeIcon />,
+          subtitle: 'A small introduction of me and my work',
+        },
+        {
+          id: 'aboutAction',
+          name: 'About',
+          shortcut: ['a'],
+          keywords: 'contact',
+          section: 'Navigation',
+          perform: () => router.push('/about'),
+        },
+        {
+          id: 'projectsAction',
+          name: 'Projects',
+          shortcut: ['p'],
+          keywords: 'work',
+          section: 'Navigation',
+          perform: () => router.push('/projects'),
+        },
+        {
+          id: 'blogAction',
+          name: 'Blog',
+          shortcut: ['p'],
+          keywords: 'post articles',
+          section: 'Navigation',
+          perform: () => router.push('/blog'),
+        },
+        {
+          id: 'githubAction',
+          name: 'GitHub',
+          shortcut: ['g', 'h'],
+          keywords: 'sourcecode',
+          section: 'Socials',
+          perform: () =>
+            window.open(
+              'https://github.com/codingcodax/portfolio-v1',
+              '_blank'
+            ),
+        },
+        {
+          id: 'linkedinAction',
+          name: 'Likedin',
+          shortcut: ['l'],
+          keywords: 'social contact dm',
+          section: 'Socials',
+          perform: () =>
+            window.open('https://www.linkedin.com/in/codax/', '_blank'),
+        },
+        {
+          id: 'twitterAction',
+          name: 'Twitter',
+          shortcut: ['t'],
+          keywords: 'social contact dm',
+          section: 'Socials',
+          perform: () =>
+            window.open('https://twitter.com/codingcodax', '_blank'),
+        },
+        {
+          id: 'youtubeAction',
+          name: 'YouTube',
+          shortcut: ['y', 't'],
+          keywords: 'videos',
+          section: 'Socials',
+          perform: () =>
+            window.open('https://twitter.com/codingcodax', '_blank'),
+        },
+      ]}
+    >
+      <ChakraProvider resetCSS theme={theme}>
+        <KBarPortal>
+          <KBarPositioner>
+            <KBarAnimator style={animator}>
+              <KBarSearch style={search} />
+              <RenderResults />
+            </KBarAnimator>
+          </KBarPositioner>
+        </KBarPortal>
+
+        <Layout>
+          <Head />
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </KBarProvider>
   );
 };
 
